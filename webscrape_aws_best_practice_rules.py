e25c843356033dce3a8f55e41dca5220dad435dd
unknown <roshan.patel771@gmail.com>
2020-09-28 08:27:55 +0800

    switched to use pandas library

--- webscrape_aws_best_practice_rules.py
+++ webscrape_aws_best_practice_rules.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import io
-
+import pandas as pd
+from pandas import ExcelWriter
+from openpyxl.workbook import Workbook
 
 # gets all the html objects that contain the relevant links
 def get_links(soup):
@@ -16,7 +18,7 @@ def get_links(soup):
     return rule_links
 
 # method for getting the required data from the list of links
-def get_data(rule_links, filename):
+def get_data(rule_links, w, row):
     for link in rule_links:
         source = requests.get(link).text
         soup = BeautifulSoup(source, 'html.parser')
@@ -70,50 +72,52 @@ def get_data(rule_links, filename):
         steps = (soup.findAll("div",{"class":"overlay"}))
 
         try:
-            audit_using_console = steps[1].text.replace(',','').replace("\n","")
+            audit_using_console = steps[1].text.replace(',','').replace('			','').strip()
         except:
             audit_using_console = ''
 
         try:
-            audit_using_cli = steps[3].text.replace(',', '').replace("\n","")
+            audit_using_cli = steps[3].text.replace(',','').replace('			','').strip()
         except:
             audit_using_cli = ''
 
         try:
-            remediation_using_console = steps[5].text.replace(',', '').replace("\n","")
+            remediation_using_console = steps[5].text.replace(',','').replace('			','').strip()
         except:
             remediation_using_console = ''
 
         try:
-            remediation_using_cli = steps[7].text.replace(',', '').replace("\n","")
+            remediation_using_cli = steps[7].text.replace(',','').replace('			','').strip()
         except:
             remediation_using_cli = ''
 
         # format the relevant data into a csv friendly string, and then write it to the file
         csv_string = title + "," + aws_service + "," + risk_level + ',' + rule_id + ',' + description + ',' + rationale + ',' + audit_goal + ',' + audit_using_console + ',' + audit_using_cli + ',' + remediation_using_console + ',' + remediation_using_cli + "," + link
         print(csv_string)
-
-        with io.open(filename, "a", encoding="utf-8") as f:
-             f.write(csv_string + "\n")
+        row = row + 1
+        # Create dataframe and write to file
+        df = pd.DataFrame(
+            columns=[title, aws_service, risk_level, rule_id, description, rationale, audit_goal, audit_using_console,
+                     audit_using_cli, remediation_using_console, remediation_using_cli, link])
+        df.to_excel(w, startrow=row, index=False)
+        w.save()
 
 def main():
     # variables: address to gather links from and variables for html parsing
     address = "https://www.cloudconformity.com/knowledge-base/aws/"
     source = requests.get(address).text
     soup = BeautifulSoup(source, 'html.parser')
-
-    # name of file to write to and headers for csv
-    filename = "aws_best_practice_rules.csv"
-    headers = "Title, AWS Service, Risk, ID, Description, Rationale, Audit Goal, Audit Console, Audit Cli, Remediation Console, Remediation Cli, Link\n"
-
-    # writes headers
-    with io.open(filename, "w", encoding="utf-8") as f:
-        f.write(headers)
-        f.close()
+    w = pd.ExcelWriter("aws_bp_rules.xlsx")
+    row = 0;
+    df = pd.DataFrame(
+        columns=["Title", "AWS Service", "Risk", "ID", "Description", "Rationale", "Audit Goal", "Audit Console",
+                 "Audit Cli", "Remediation Console", "Remediation Cli", "Link"])
+    df.to_excel(w, startrow=row, index=False)
+    w.save()
 
     # calls method to get all rule links from the 'address' variables and iterates through the links to get required data
     links = get_links(soup)
-    get_data(links, filename)
+    get_data(links, w, row)
 
 if __name__ == '__main__':
     main()
--- webscrape_azure_best_practice_rules.py
+++ webscrape_azure_best_practice_rules.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 import io
-
+import pandas as pd
+from pandas import ExcelWriter
+from openpyxl.workbook import Workbook
 
 # gets all the html objects that contain the relevant links
+
 def get_links(soup):
     link_containers = soup.find_all("li", {"class":"rule-link"})
     rule_links = []
@@ -16,11 +19,10 @@ def get_links(soup):
     return rule_links
 
 # method for getting the required data from the list of links
-def get_data(rule_links, filename):
+def get_data(rule_links, w, row):
     for link in rule_links:
         source = requests.get(link).text
         soup = BeautifulSoup(source, 'html.parser')
-
         try:
             azure_service = soup.find("div", {"class":"nav"}).text.strip()
 
@@ -70,52 +72,50 @@ def get_data(rule_links, filename):
         steps = (soup.findAll("div",{"class":"overlay"}))
 
         try:
-            audit_using_console = steps[1].text.replace(',','').replace("\n","")
+            audit_using_console = steps[1].text.replace(',','').replace('			','').strip()
         except:
             audit_using_console = ''
 
         try:
-            audit_using_cli = steps[3].text.replace(',', '').replace("\n","")
+            audit_using_cli = steps[3].text.replace(',', '').replace('			','').strip()
         except:
             audit_using_cli = ''
 
         try:
-            remediation_using_console = steps[5].text.replace(',', '').replace("\n","")
+            remediation_using_console = steps[5].text.replace(',', '').replace('			','').strip()
         except:
             remediation_using_console = ''
 
         try:
-            remediation_using_cli = steps[7].text.replace(',', '').replace("\n","")
+            remediation_using_cli = steps[7].text.replace(',', '').replace('			','').strip()
         except:
             remediation_using_cli = ''
 
         # format the relevant data into a csv friendly string, and then write it to the file
         csv_string = title + "," + azure_service + "," + risk_level + ',' + rule_id + ',' + description + ',' + rationale + ',' + audit_goal + ',' + audit_using_console + ',' + audit_using_cli + ',' + remediation_using_console + ',' + remediation_using_cli + "," + link
         print(csv_string)
-
-        with io.open(filename, "a", encoding="utf-8") as f:
-             f.write(csv_string + "\n")
-
-
+        row = row + 1
+        # Create dataframe and write to file
+        df = pd.DataFrame(columns=[title,azure_service,risk_level,rule_id,description,rationale,audit_goal,audit_using_console,audit_using_cli, remediation_using_console, remediation_using_cli, link])
+        df.to_excel(w, startrow=row, index=False)
+        w.save()
 
 def main():
     # variables: address to gather links from and variables for html parsing
     address = "https://www.cloudconformity.com/knowledge-base/azure/"
     source = requests.get(address).text
     soup = BeautifulSoup(source, 'html.parser')
-
-    # name of file to write to and headers for csv
-    filename = "azure_best_practice_rules.csv"
-    headers = "Title, Azure Service, Risk, ID, Description, Rationale, Audit Goal, Audit Console, Audit Cli, Remediation Console, Remediation Cli, Link\n"
-
-    # writes headers
-    with io.open(filename, "w", encoding="utf-8") as f:
-        f.write(headers)
-        f.close()
+    w = pd.ExcelWriter("azure_bp_rules.xlsx")
+    row = 0;
+    df = pd.DataFrame(columns=["Title", "Azure Service", "Risk", "ID", "Description", "Rationale", "Audit Goal", "Audit Console", "Audit Cli", "Remediation Console", "Remediation Cli", "Link"])
+    df.to_excel(w, startrow=row, index=False)
+    w.save()
 
     # calls method to get all rule links from the 'address' variables and iterates through the links to get required data
     links = get_links(soup)
-    get_data(links, filename)
+    get_data(links, w, row)
+
+
 
 if __name__ == '__main__':
     main()
